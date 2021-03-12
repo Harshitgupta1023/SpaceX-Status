90a6677351d48fc95c38e517f4d4754220368ea4
Harshit Gupta <harshitgupta1023@gmail.com>
2021-03-13 05:29:51 +0530

    deployed server

---
+++ schema.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
+} = require("graphql");
+
+const LaunchType = new GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    details: { type: GraphQLString },
+    date_local: { type: GraphQLString },
+    success: { type: GraphQLBoolean },
+    rocket: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v4/launches")
+          .then((res) => res.data);
+      },
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios.get(
+          `https://api.spacexdata.com/v4/launches/${flight_number}`.then(
+            (res) => res.data
+          )
+        );
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
